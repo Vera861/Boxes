036522541436ab9a644f1c43a84673c093d1ee60
Vera <mverynchik@gmail.com>
2021-05-03 17:06:14 +0300

    1 && 2 paragraph home work

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TestType arr1 = new TestType("Hello", "my", "small", "lovely", "kitty");
+        arr1.reformat();
+        arr1.swapElements(3, 4);
+        System.out.println();
+        arr1.showArr();
+    }
+
+}
---
+++ src/main/java/TestType.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class TestType<T> {
+    private T[] arr;
+
+    public TestType(T... arr) {
+        this.arr = arr;
+    }
+
+    public void swapElements(int i, int j) {
+        T elem;
+        elem = arr[i - 1];
+        arr[i - 1] = arr[j - 1];
+        arr[j - 1] = elem;
+    }
+
+    public void showArr(){
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public void reformat() {
+        ArrayList<T> newFormat = new ArrayList<>();
+        for (T a : arr) {
+            newFormat.add(a);
+        }
+        System.out.println(newFormat);
+    }
+
+}
